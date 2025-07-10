9658852a45f29037c233e00f17d5e55038d31598
Lam Nguyen <nguyentamhoanglam@gmail.com>
2025-07-10 10:23:08 +0000

    add replay buffer
    Signed-off-by: Lam Nguyen <nguyentamhoanglam@gmail.com>

--- replay_buffer.py
+++ replay_buffer.py
@@ -5,8 +5,26 @@ import torch
 import torch.nn.functional as F
 
 
+def zero_pad_sequences(
+    sequences: list[torch.Tensor], side: str = "left"
+) -> torch.Tensor:
+    """
+    pad sequences with 0 on either left or right
+
+    """
+    assert side in ("left", "right")
+    max_len = max(seq.size(0) for seq in sequences)
+    padded_sequences = []
+    for seq in sequences:
+        pad_len = max_len - seq.size(0)
+        padding = (pad_len, 0) if side == "left" else (0, pad_len)
+        padded_sequences.append(F.pad(seq, padding))
+    return torch.stack(padded_sequences, dim=0)
+
+
 @dataclass
 class Experience:
+    sequences: torch.Tensor
     action_log_probs: torch.Tensor
     log_probs_ref: torch.Tensor
     advantages: Optional[torch.Tensor]
@@ -21,3 +39,71 @@ class Experience:
                 v = v.to(device=device)
             members[field.name] = v
         return Experience(**members)
+
+
+def split_experience_batch(experience: Experience) -> list[Experience]:
+    """split an experience to a batch of experiences"""
+    batch_size = experience.sequences.size(0) # split in first dimension
+    batch_data = [{} for _ in range(batch_size)]
+    keys = (
+        "sequences",
+        "action_log_probs",
+        "log_probs_ref",
+        "advantages",
+        "action_mask",
+    )
+    for key in keys:
+        value = getattr(experience, key)
+        if value is None:
+            vals = [None] * batch_size
+        else:
+            vals = torch.unbind(value)
+        for i, v in enumerate(vals):
+            batch_data[i][key] = v
+    return [Experience(**data) for data in batch_data]
+
+
+def join_experience_batch(items: list[Experience]) -> Experience:
+    batch_data = {}
+    keys = (
+        "sequences",
+        "action_log_probs",
+        "log_probs_ref",
+        "advantages",
+        "action_mask",
+    )
+    for key in keys:
+        vals = [getattr(item, key) for item in items]
+        if all(v is not None for v in vals):
+            data = 0
+        else:
+            data = None
+        batch_data[key] = data
+    return Experience(**batch_data)
+
+
+class ReplayBuffer:
+    def __init__(self, limit: int = 0) -> None:
+        self.limit = limit
+        self.items = list[Experience] = []
+    
+    def append(self, experience: Experience) -> None:
+        """add an experience to pool"""
+        items = split_experience_batch(experience)
+        self.items.extend(items)
+        if self.limit > 0:
+            samples_to_remove = len(self.items) - self.limit
+            if samples_to_remove > 0:
+                # keep top-k latest experiences
+                self.items = self.items[samples_to_remove:]
+    
+    def clear(self) -> None:
+        # clear experiences
+        self.items.clear()
+
+    def __len__(self) -> int:
+        return len(self.items)
+
+    def __getitem__(self, idx: int) -> Experience:
+        # get a specific experience at index
+        return self.items[idx]
